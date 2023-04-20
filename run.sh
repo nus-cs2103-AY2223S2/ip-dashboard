5eaab6d6cdd938e17c5f3b6cc0b2e1290e8ac68b
Damith C. Rajapakse <damith@gmail.com>
2023-04-20 11:04:28 +0800

    Revert back to using master branch

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-./get-reposense.py --commit 7a58117a78c2432cea116871a7ce5c37abe6409c 
+./get-reposense.py --master 
 java -jar RepoSense.jar --config ./configs --since 13/01/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
